19b4627162c0a70429afd759ef44ec8c0e4c1b2d
J. Blumenkamp <jb2270@cam.ac.uk>
2021-01-11 20:50:18 +0000

    Turtlebot lidar, single agent training and hyperparameters works

--- train_single.py
+++ train_single.py
@@ -1,47 +1,58 @@
-from turtlebot_single import SimEnv
+from turtlebot import CentrSimEnv
 import time
 import numpy as np
 import yaml
 import ray
 from ray import tune
 from ray.tune.registry import register_env
+from ray.tune.logger import pretty_print, DEFAULT_LOGGERS, TBXLogger
+from ray.tune.integration.wandb import WandbLogger
 
 from ray.rllib.models import ModelCatalog
-from adversarial_comms.trainers.multiagent_ppo import MultiPPOTrainer
-from adversarial_comms.trainers.hom_multi_action_dist import TorchHomogeneousMultiActionDistribution
+#from adversarial_comms.trainers.multiagent_ppo import MultiPPOTrainer
+#from adversarial_comms.trainers.hom_multi_action_dist import TorchHomogeneousMultiActionDistribution
 # Can alternatively pass in p.DIRECT
 
-if __name__ == '__main__':
-    register_env("pybullet", lambda config: SimEnv(config))
+import random
 
-    ray.init()
+def train():
     tune.run(
         "PPO",
         #restore="/home/jb2270/ray_results/PPO/PPO_world_0_2020-04-04_23-01-16c532w9iy/checkpoint_100/checkpoint-100",
-        checkpoint_freq=10,
+        checkpoint_freq=1,
+        keep_checkpoints_num=2,
+        #local_dir="/tmp",
+        loggers=DEFAULT_LOGGERS + (WandbLogger,),
         config={
             "framework": "torch",
-            "_use_trajectory_view_api": False,
-            "env": "pybullet",
-            "lambda": 0.95,
-            "kl_coeff": 0.5,
-            "clip_rewards": True,
+            "env": "pybullet_centr", #"pybullet_centr",
+            #"lambda": 0.95,
+            "kl_coeff": 0.0,
             "clip_param": 0.2,
-            "entropy_coeff": 0.01,
-            "train_batch_size": 1000,
-            "rollout_fragment_length": 100,
-            "sgd_minibatch_size": 500,
-            "num_sgd_iter": 10,
-            "num_workers": 1,
+            "entropy_coeff": 0.001,
+            "train_batch_size": 100000,
+            "sgd_minibatch_size": 16384,
+            "num_sgd_iter": 20,
+            "num_gpus": 0.5,
+            "num_workers": 16,
+            #"num_gpus_per_worker": 0.5/num_workers,
             "num_envs_per_worker": 1,
             "lr": 1e-4,
-            "gamma": 0.99,
-            "batch_mode": "truncate_episodes",
+            "gamma": 0.995,
+            "batch_mode": "complete_episodes", # complete_episodes, truncate_episodes
             "observation_filter": "NoFilter",
-            "num_gpus": 0,
+            #"normalize_actions": True,
+            #"grad_clip": 0.5,
             "model": {
                 "fcnet_activation": "relu",
                 "fcnet_hiddens": [128, 256, 128, 32],
+            },
+            "logger_config": {
+                "wandb": {
+                    "project": "rl_dynamic_control",
+                    "group": "turtlebots",
+                    "api_key_file": "./wandb_api_key_file"
+                }
             },
             "env_config": {
                 'agent_poses': [
@@ -52,37 +63,16 @@ if __name__ == '__main__':
                     [0.3, 0.5, 0],
                     #[-0.3, 0.5, 0]
                 ],
-                'max_time_steps': 100,
+                'max_time_steps': 3000,
                 'communication_range': 2.0,
                 'render': False,
             }})
 
-    #tune.run(
-    #    "SAC",
-        #restore="/home/jb2270/ray_results/PPO/PPO_world_0_2020-04-02_21-44-56vzulq1wd/checkpoint_190",
-        #checkpoint_freq=10,
-    #    config=cfg)
 
-'''
-d = Drone()
-d.set_speed([0,0,0.01])
-for i in range(200):
-    d.step()
-    p.stepSimulation()
-    time.sleep(1./240.)
+if __name__ == '__main__':
+    register_env("pybullet", lambda config: SimEnv(config))
+    register_env("pybullet_centr", lambda config: CentrSimEnv(config))
 
-d.set_speed([0,0.01,0.0])
+    ray.init(num_cpus=32*4, num_gpus=1)
+    train()
 
-for i in range(1000):
-    d.step()
-    p.stepSimulation()
-    time.sleep(1./240.)
-
-d.set_speed([0,0,-0.005])
-for i in range(1000):
-    d.step()
-    p.stepSimulation()
-    time.sleep(1./240.)
-#time.sleep(5)
-p.disconnect()
-'''
--- turtlebot.py
+++ turtlebot.py
@@ -3,13 +3,20 @@ import pybullet_data
 import time
 import gym
 import numpy as np
+from pathlib import Path
 
 class Turtlebot():
     CONFIG = {
         'limits': {
             'forward_speed': (-0.2, 0.2),
-            'yaw_rate': (-np.pi/8, np.pi/8),
+            'yaw_rate': (-np.pi/8, np.pi/8)
         },
+        'lidar': {
+            'range': 4,
+            'rays': 8,
+            'rays_start': -np.pi/2,
+            'rays_end': np.pi/2,
+        }
     }
 
     def __init__(self, pybullet_client, reset_pos, goal_pos):
@@ -17,17 +24,25 @@ class Turtlebot():
         self.initial_pos = self.position = np.array(reset_pos)
         self.goal_pos = np.array(goal_pos)
         self.body_id = p.loadURDF("turtlebot3.urdf", basePosition=self.initial_pos, physicsClientId=self.pybullet_client)
+        self.lidar_lines = [-1]*16
+        self.lidar_base = np.array([-0.03, 0, 0.18])
 
         self.reset()	
 
     def reset(self):
+        self.lateral_speed = np.array([0,0,0])
+        self.ang_speed = np.array([0,0,0])
+
         p.resetBasePositionAndOrientation(self.body_id, self.initial_pos, p.getQuaternionFromEuler([0,0,90*(np.pi/180)]), physicsClientId=self.pybullet_client)
         self.update_state()
-        self.closest_dist_to_goal = np.sqrt(np.sum((self.position - self.goal_pos)**2))
         self.setpoint_forward_speed = 0
         self.setpoint_yaw_rate = 0
+        self.reached_middle = False
 
     def update_state(self):
+        self.last_lateral_speed = self.lateral_speed.copy()
+        self.last_ang_speed = self.ang_speed.copy()
+
         position, orientation = p.getBasePositionAndOrientation(self.body_id, physicsClientId=self.pybullet_client)
         self.position = np.array(position)
         self.orientation = np.array(orientation)
@@ -63,24 +78,55 @@ class Turtlebot():
         p.setJointMotorControl2(self.body_id, 1, p.VELOCITY_CONTROL, targetVelocity=v_r, force=1000, physicsClientId=self.pybullet_client)
         p.setJointMotorControl2(self.body_id, 2, p.VELOCITY_CONTROL, targetVelocity=v_l, force=1000, physicsClientId=self.pybullet_client)
 
-        origin_pose = np.array([0, 0, 0])
         m = self.get_rotation_matrix()
         goal_relative = (self.goal_pos - self.position) @ m
-        #gap_relative = (origin_pose - self.position) @ m
-        speed = np.array([self.lateral_speed[0], self.ang_speed[2]])
-        obs = np.hstack([self.position, goal_relative, speed])
-        dst_goal = np.sqrt(np.sum(goal_relative**2))
-        #reward = -dst_goal
-        reward = 0
-        if dst_goal < self.closest_dist_to_goal:
-            reward = self.closest_dist_to_goal - dst_goal
-            self.closest_dist_to_goal = dst_goal
-        done = dst_goal < 0.1
+        gap_relative = (np.array([0, 0, 0]) - self.position) @ m
 
-        if not np.all(np.isfinite(obs)) or not np.isfinite(reward):
-             import pdb; pdb.set_trace()
+        cfg_lidar = self.CONFIG['lidar']
+        rays_range = cfg_lidar['rays_end'] - cfg_lidar['rays_start']
+        base = self.position + m @ self.lidar_base
+        start = np.pi/2 + cfg_lidar['rays_start']
+        lidar_rays = p.rayTestBatch(
+            [base]*cfg_lidar['rays'],
+            [base + m @ np.array([
+                cfg_lidar['range']*np.sin(start + i*rays_range/cfg_lidar['rays']),
+                cfg_lidar['range']*np.cos(start + i*rays_range/cfg_lidar['rays']),
+                0]) for i in range(cfg_lidar['rays'])
+            ]
+        )
+        self.lidar_rays = [v[2] for v in lidar_rays]
 
-        return obs, reward, done
+        obs = np.hstack([
+            self.lateral_speed,
+            self.ang_speed,
+            #self.last_lateral_speed,
+            #self.last_ang_speed,
+            #np.sin(self.orientation_euler),
+            #np.cos(self.orientation_euler),
+            #gap_relative,
+            goal_relative,
+            self.lidar_rays
+        ])
+
+        return obs
+
+    def render_lidar(self):
+        cfg_lidar = self.CONFIG['lidar']
+        rays_range = cfg_lidar['rays_end'] - cfg_lidar['rays_start']
+        m = self.get_rotation_matrix()
+        base = self.position + m @ self.lidar_base
+        start = np.pi/2 + cfg_lidar['rays_start']
+        for i, r in enumerate(self.lidar_rays):
+            self.lidar_lines[i] = p.addUserDebugLine(
+                base,
+                base + m @ np.array([
+                    r*cfg_lidar['range']*np.sin(start + i*rays_range/len(self.lidar_rays)),
+                    r*cfg_lidar['range']*np.cos(start + i*rays_range/len(self.lidar_rays)),
+                    0
+                ]),
+                (1,0,0),
+                replaceItemUniqueId=self.lidar_lines[i]
+            )
 
 class SimEnv(gym.Env):
     def __init__(self, config):
@@ -93,18 +139,20 @@ class SimEnv(gym.Env):
             # current pose relative to goal (x,y)
             # current pose relative to origin (and therefore gap in wall) (x, y, phi)
             # current velocity (lin, ang)
-            'agent_obs': gym.spaces.Tuple(
-                (gym.spaces.Box(-10000, 10000, shape=(8,), dtype=float),)*n_agents
-            ),
+            'agents': gym.spaces.Tuple((
+                gym.spaces.Dict({
+                    "obs": gym.spaces.Box(-10000, 10000, shape=(9+8,), dtype=float),
+                    "state": gym.spaces.Box(low=-10000, high=10000, shape=(9,))
+                })
+            ,)*n_agents),
             'gso': gym.spaces.Box(-1, 1, shape=(n_agents, n_agents), dtype=float),
-            # agent obs and current absolute pose
-            'state': gym.spaces.Box(low=-10000, high=10000, shape=(n_agents, 11)),
         })
 
         self.client = p.connect(p.GUI if self.cfg['render'] else p.DIRECT)
         p.setGravity(0, 0, -9.81, physicsClientId=self.client)
         p.setAdditionalSearchPath(pybullet_data.getDataPath())
         self.plane_id = p.loadURDF("plane.urdf", physicsClientId=self.client)
+        p.setAdditionalSearchPath(str(Path(__file__).parent / "data"))
         self.wall_id = p.loadURDF("wall.urdf", physicsClientId=self.client)
 
         self.robots = []
@@ -143,34 +191,51 @@ class SimEnv(gym.Env):
 
     def step(self, actions):
         self.timestep += 1
-        obs, dones, infos = [], [], {'rewards': {}}
-        reward = 0
+        observations, dones, infos = [], [], {'rewards': {}}
+        all_reward = 0
         for i, (robot, action) in enumerate(zip(self.robots, actions)):
             robot.set_velocity(action[0], action[1])
-            o, r, d = robot.step()
-            obs.append(o)
-            dones.append(d)
-            infos['rewards'][i] = r
-            reward += r
+            observation = robot.step()
 
-        for _ in range(10):
-            p.stepSimulation(physicsClientId=self.client)
+            goal_vector = robot.goal_pos - robot.position
+            reward = np.dot(goal_vector/np.linalg.norm(goal_vector), robot.world_lateral_speed)*np.linalg.norm(robot.world_lateral_speed)
+            done = np.sqrt(np.sum(goal_vector**2)) < 0.1
+            if done:
+                reward = 200
+            collision_idx = set([o[2] for o in p.getContactPoints(bodyA=robot.body_id, physicsClientId=self.client)])
+            if self.wall_id in collision_idx:
+                reward -= 0.1
+            if any([not (i == self.wall_id) and not (i == self.plane_id) for i in collision_idx]):
+                reward -= 0.01
 
-        state = []
-        for r, o in zip(self.robots, obs):
-            state.append(np.concatenate([o, np.array(r.position)]))
-        obs = {
-            'agent_obs': tuple(obs),
+            if not np.all(np.isfinite(observation)) or not np.isfinite(reward):
+                import pdb; pdb.set_trace()
+
+            observations.append({
+                "obs": observation,
+                "state": np.hstack([
+                    robot.position,
+                    np.sin(robot.orientation_euler),
+                    np.cos(robot.orientation_euler)
+                ])
+            })
+            dones.append(done)
+            infos['rewards'][i] = reward
+            all_reward += reward
+
+        #self.robots[0].render_lidar()
+
+        p.stepSimulation(physicsClientId=self.client)
+
+        all_obs = {
+            'agents': tuple(observations),
             'gso': self.compute_gso(),
-            'state': np.array(state)
         }
-        if not np.all(np.isfinite(obs['state'])) or not np.all(np.isfinite(obs['gso'])):
-             import pdb; pdb.set_trace()
+        #if not np.all(np.isfinite(obs['state'])) or not np.all(np.isfinite(obs['gso'])):
+        #     import pdb; pdb.set_trace()
 
-        #print(self.timestep)
-        done = all(dones) or self.timestep > self.cfg['max_time_steps']
-        #print("INF", actions, infos, len(self.robots))
-        return obs, reward, done, infos
+        done = any(dones) or self.timestep > self.cfg['max_time_steps']
+        return all_obs, all_reward, done, infos
 
 class CentrSimEnv(gym.Env):
     def __init__(self, config):
@@ -178,12 +243,13 @@ class CentrSimEnv(gym.Env):
         n_agents = len(self.cfg['agent_poses'])
         self.action_space = gym.spaces.Box(low=np.array([-np.pi/8, -0.2]*n_agents), high=np.array([np.pi/8, 0.2]*n_agents), shape=(2*n_agents,), dtype=float)
 
-        self.observation_space = gym.spaces.Box(-10000, 10000, shape=(8*n_agents,), dtype=float)
+        self.observation_space = gym.spaces.Box(-10000, 10000, shape=((9+8)*n_agents,), dtype=float)
 
         self.client = p.connect(p.GUI if self.cfg['render'] else p.DIRECT)
         p.setGravity(0, 0, -9.81, physicsClientId=self.client)
         p.setAdditionalSearchPath(pybullet_data.getDataPath())
         self.plane_id = p.loadURDF("plane.urdf", physicsClientId=self.client)
+        p.setAdditionalSearchPath(str(Path(__file__).parent / "data"))
         self.wall_id = p.loadURDF("wall.urdf", physicsClientId=self.client)
 
         self.robots = []
@@ -203,20 +269,42 @@ class CentrSimEnv(gym.Env):
 
     def step(self, actions):
         self.timestep += 1
-        obs, dones = [], []
-        reward = 0
-        for i, (robot, action) in enumerate(zip(self.robots, np.array(actions).reshape(-1, len(self.robots)))):
+        observations, dones = [], []
+        all_reward = 0
+        #import pdb; pdb.set_trace()
+        for i, (robot, action) in enumerate(zip(self.robots, np.array(actions).reshape(len(self.robots), -1))):
             robot.set_velocity(action[0], action[1])
-            o, r, d = robot.step()
-            obs.append(o)
-            dones.append(d)
-            reward += r
+            observation = robot.step()
 
-        for _ in range(10):
-            p.stepSimulation(physicsClientId=self.client)
+            goal_vector = robot.goal_pos - robot.position
+            reward = np.dot(goal_vector/np.linalg.norm(goal_vector), robot.world_lateral_speed)*np.linalg.norm(robot.world_lateral_speed)
+            #if not robot.reached_middle and robot.position[1] > 0:
+            #    reward = 5
+            #    robot.reached_middle = True
 
+            done = np.sum(goal_vector**2) < 0.05**2
+            if done:
+                reward = 200
+            collision_idx = set([o[2] for o in p.getContactPoints(bodyA=robot.body_id, physicsClientId=self.client)])
+            if self.wall_id in collision_idx:
+                reward -= 0.1
+                #done = True
+            #if any([not (i == self.wall_id) and not (i == self.plane_id) for i in collision_idx]):
+            #    reward -= 0.01
 
-        done = all(dones) or self.timestep > self.cfg['max_time_steps']
-        #print("INF", actions, infos, len(self.robots))
-        return np.concatenate(obs), reward, done, {}
+            if not np.all(np.isfinite(observation)) or not np.isfinite(reward):
+                import pdb; pdb.set_trace()
+
+            #if reward < 1e-5:
+            #    reward = 0
+            observations.append(observation)
+            dones.append(done)
+            all_reward += reward
+
+        #self.robots[0].render_lidar()
+
+        p.stepSimulation(physicsClientId=self.client)
+
+        done = any(dones) or self.timestep > self.cfg['max_time_steps']
+        return np.concatenate(observations), all_reward, done, {}
 
