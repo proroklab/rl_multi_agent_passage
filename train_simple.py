a7b33826fee9441d1d6dc2aae1914682c6d99f7f
J. Blumenkamp <jb2270@cam.ac.uk>
2021-02-01 12:20:51 +0000

    Add simple test scenario in 2D continuous env, single agent

---
+++ train_simple.py
@@ -0,0 +1,76 @@
+#from turtlebot import SimEnv
+from simple_env import SimpleEnv
+import time
+import numpy as np
+import yaml
+import ray
+from ray import tune
+from ray.tune.registry import register_env
+
+from ray.tune.logger import pretty_print, DEFAULT_LOGGERS, TBXLogger
+from ray.tune.integration.wandb import WandbLogger
+
+from model import Model
+from model_2 import Model as Model2
+from ray.rllib.models import ModelCatalog
+from adversarial_comms.trainers.multiagent_ppo import MultiPPOTrainer
+from adversarial_comms.trainers.hom_multi_action_dist import TorchHomogeneousMultiActionDistribution
+
+if __name__ == '__main__':
+    #ray.init(local_mode=True)
+    ray.init()
+
+    register_env("simple", lambda config: SimpleEnv(config))
+    ModelCatalog.register_custom_model("model", Model)
+    ModelCatalog.register_custom_model("model2", Model2)
+    ModelCatalog.register_custom_action_dist("hom_multi_action", TorchHomogeneousMultiActionDistribution)
+
+    num_workers = 16
+    tune.run(
+        MultiPPOTrainer,
+        #restore="/home/jb2270/ray_results/PPO/PPO_world_0_2020-04-04_23-01-16c532w9iy/checkpoint_100/checkpoint-100",
+        checkpoint_freq=1,
+        keep_checkpoints_num=2,
+        checkpoint_score_attr="episode_reward_mean",
+        #local_dir="/tmp",
+        loggers=DEFAULT_LOGGERS + (WandbLogger,),
+        config={
+            "framework": "torch",
+            "env": "simple",
+            #"lambda": 0.95,
+            "clip_param": 0.2,
+            "entropy_coeff": 0.001,
+            "train_batch_size": 5000,
+            "sgd_minibatch_size": 1024,
+            "num_sgd_iter": 10,
+            "num_gpus": 0.5,
+            "num_workers": 16,
+            #"num_gpus_per_worker": 0.5/num_workers,
+            "num_envs_per_worker": 1,
+            "lr": 1e-4,
+            "gamma": 0.995,
+            "batch_mode": "complete_episodes", # complete_episodes, truncate_episodes
+            "observation_filter": "NoFilter",
+            "model": {
+                "custom_model": "model2",
+                "custom_action_dist": "hom_multi_action",
+                "custom_model_config": {
+                    "graph_tabs": 2,
+                    "activation": "relu",
+                }
+            },
+            "logger_config": {
+                "wandb": {
+                    "project": "rl_dynamic_control",
+                    "group": "turtlebots",
+                    "api_key_file": "./wandb_api_key_file"
+                }
+            },
+            "env_config": {
+                'world_shape': (3., 3.),
+                'agent_formation': [[-0.2, 0], [0.2, 0]], #[-0.2, -0.2], [-0.2, 0.2], [0.2, -0.2], [0.2, 0.2]],
+                'max_time_steps': 300,
+                'communication_range': 2.0,
+                'render': False,
+            }})
+
